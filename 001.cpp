5bc4d79b8ecd529bab977a28e168f91bbd387b77
eftrahman <83782669+eftrahman@users.noreply.github.com>
2024-05-18 23:39:36 +0600

    second commit

--- 001.cpp
+++ 001.cpp
@@ -1,2 +1,22 @@
-//Rinzler 13th May 2024
+//Rinzler 13th May 2023
 //LeetCode Problem 01
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return vector<int>{i, j};
+                }
+            }
+        }
+
+        return vector<int>();
+    }
+};
