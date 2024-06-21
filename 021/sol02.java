2049d9282e5feea31ed63f0db1535c4348680f64
Rinzler <83782669+eftrahman@users.noreply.github.com>
2024-06-21 23:23:54 +0600

    Create sol02.java

---
+++ 021/sol02.java
@@ -0,0 +1,63 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+
+        if (l1 != null) {
+            tail.next = l1;
+        } else {
+            tail.next = l2;
+        }
+
+        return dummy.next;
+    }
+
+    // Helper functions to create and print linked lists for testing purposes
+    public static ListNode createList(int[] values) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        for (int val : values) {
+            current.next = new ListNode(val);
+            current = current.next;
+        }
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        // Test case 1
+        ListNode l1 = createList(new int[]{1, 2, 4});
+        ListNode l2 = createList(new int[]{1, 3, 4});
+        ListNode result = solution.mergeTwoLists(l1, l2);
+        printList(result); // Expected output: 1 1 2 3 4 4
+    }
+}
