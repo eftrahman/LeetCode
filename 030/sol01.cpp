9468bbb5d6015642b7b70df351f28b1ff1ec7815
Rinzler <83782669+eftrahman@users.noreply.github.com>
2024-07-01 22:54:52 +0600

    Update sol01.cpp

--- 030/sol01.cpp
+++ 030/sol01.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        vector<int> result;
+        if (s.empty() || words.empty()) return result;
+
+        int wordLength = words[0].length();
+        int wordsCount = words.size();
+        int substringLength = wordLength * wordsCount;
+
+        unordered_map<string, int> wordsMap;
+        for (const string& word : words) {
+            wordsMap[word]++;
+        }
+
+        for (int i = 0; i <= s.length() - substringLength; i++) {
+            unordered_map<string, int> seen;
+            int j = 0;
+            for (; j < wordsCount; j++) {
+                int wordStartIndex = i + j * wordLength;
+                string word = s.substr(wordStartIndex, wordLength);
+                if (wordsMap.find(word) != wordsMap.end()) {
+                    seen[word]++;
+                    if (seen[word] > wordsMap[word]) {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+            if (j == wordsCount) {
+                result.push_back(i);
+            }
+        }
+
+        return result;
+    }
+};
 
